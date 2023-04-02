72ddbf9542bdad38281e565265ce99332d448657
Ong <ong2323@gmail.com>
2023-04-01 21:19:05 -0400

    initial commit

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './popup.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./popup.css";
+import { Box, Paper, Card, Typography, CardContent } from "@mui/material";
 
 const App: React.FC<{}> = () => {
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
-  )
-}
+    <Box>
+      <Card>
+        <CardContent>
+          <Typography display="inline" className="focus-timer" variant="h2">
+            00:00
+          </Typography>
+          <Typography display="inline" className="rest-timer">
+            00:00
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(<App />, root)
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
