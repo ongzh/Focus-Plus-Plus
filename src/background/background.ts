36620a3ec71248a659462656ea1d7cd0f9736724
Ong <ong2323@gmail.com>
2023-04-16 19:50:42 -0400

    added context menus, enable timer from context menu

--- src/background/background.ts
+++ src/background/background.ts
@@ -27,7 +27,42 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create("focusTimer", {
     periodInMinutes: 1 / 60,
   });
+
+  chrome.contextMenus.create({
+    contexts: ["page"],
+    title: "Focus++ Menu",
+    id: "focusMenu",
+  });
+
+  chrome.contextMenus.create({
+    contexts: ["page"],
+    title: "Start Focus Session",
+    id: "focusTimer",
+    parentId: "focusMenu",
+  });
+
+  chrome.contextMenus.create({
+    contexts: ["page"],
+    title: "Reset Focus Session",
+    id: "resetTimer",
+    parentId: "focusMenu",
+  });
+
+  chrome.contextMenus.create({
+    contexts: ["page"],
+    title: "End Focus Session",
+    id: "endSession",
+    parentId: "focusMenu",
+  });
+
+  chrome.contextMenus.create({
+    contexts: ["page"],
+    title: "Options",
+    id: "focusOptions",
+    parentId: "focusMenu",
+  });
 });
+
 const registration = globalThis?.registration;
 
 chrome.alarms.onAlarm.addListener((alarm) => {
@@ -88,6 +123,94 @@ chrome.alarms.onAlarm.addListener((alarm) => {
           restTimer,
         });
       });
+    });
+  }
+});
+
+function updateContextMenuTimer(isRunning: boolean) {
+  chrome.contextMenus.update("focusTimer", {
+    title: isRunning ? "Pause Focus Session" : "Start Focus Session",
+  });
+}
+
+function updateContextMenuEndSession(isFocusing: boolean) {
+  chrome.contextMenus.update("endSession", {
+    title: isFocusing ? "End Focus Session" : "End Rest Session",
+  });
+}
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "focusTimer") {
+    getStoredTimerStatus().then((timerStatus) => {
+      getStorageOptions().then((options) => {
+        if (timerStatus.isFocusing || timerStatus.isResting) {
+          setStoredTimerStatus({
+            isFocusing: false,
+            isResting: false,
+            timer: timerStatus.timer,
+            restTimer: timerStatus.restTimer,
+          });
+          updateContextMenuTimer(false);
+        } else if (options.focusTime * 60 <= timerStatus.timer) {
+          setStoredTimerStatus({
+            isFocusing: false,
+            isResting: true,
+            timer: timerStatus.timer,
+            restTimer: timerStatus.restTimer,
+          });
+          updateContextMenuTimer(true);
+        } else {
+          setStoredTimerStatus({
+            isFocusing: true,
+            isResting: false,
+            timer: timerStatus.timer,
+            restTimer: timerStatus.restTimer,
+          });
+          updateContextMenuTimer(true);
+        }
+      });
+    });
+  }
+
+  if (info.menuItemId === "resetTimer") {
+    setStoredTimerStatus({
+      isFocusing: false,
+      isResting: false,
+      timer: 0,
+      restTimer: 0,
+    });
+    updateContextMenuTimer(false);
+  }
+
+  if (info.menuItemId === "endSession") {
+    getStoredTimerStatus().then((timerStatus) => {
+      getStorageOptions().then((options) => {
+        if (timerStatus.isFocusing) {
+          setStoredTimerStatus({
+            isFocusing: timerStatus.isFocusing,
+            isResting: timerStatus.isResting,
+            timer: options.focusTime * 60,
+            restTimer: 0,
+          });
+          updateContextMenuTimer(true);
+          updateContextMenuEndSession(false);
+        } else if (timerStatus.isResting) {
+          setStoredTimerStatus({
+            isFocusing: timerStatus.isFocusing,
+            isResting: timerStatus.isResting,
+            timer: 0,
+            restTimer: options.restTime * 60,
+          });
+          updateContextMenuTimer(false);
+          updateContextMenuEndSession(true);
+        }
+      });
+    });
+  }
+
+  if (info.menuItemId === "focusOptions") {
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("options.html"),
     });
   }
 });
