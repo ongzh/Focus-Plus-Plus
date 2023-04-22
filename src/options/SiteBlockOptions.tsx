df8d5715cba526775b10fe25167093cf19c79bcf
Ong <ong2323@gmail.com>
2023-04-22 15:33:54 -0400

    site block icons

--- src/options/SiteBlockOptions.tsx
+++ src/options/SiteBlockOptions.tsx
@@ -1,5 +1,46 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Grid,
+  Switch,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
+import { SiteName } from "../utils/block";
+import { FaFacebookSquare } from "react-icons/fa";
+import { IconType } from "react-icons";
+const SiteToggle: React.FC<{
+  siteName: SiteName;
+  siteBlocked: boolean;
+  siteIcon: IconType;
+  handleBlockOption: (siteName: SiteName) => void;
+}> = ({ siteName, siteIcon, siteBlocked, handleBlockOption }) => {
+  return (
+    <Grid item>
+      <FormControl sx={{ m: 1 }} variant="outlined">
+        <FormControlLabel
+          control={
+            <Switch
+              color="primary"
+              checked={siteBlocked}
+              onChange={() => {
+                handleBlockOption(siteName);
+              }}
+            />
+          }
+          label="Notifications"
+          labelPlacement="start"
+          style={{ width: "25ch" }}
+        />
+        <FormHelperText sx={{ ml: 1 }}>
+          Chrome notifications have to be enabled in order to receive
+          notifications
+        </FormHelperText>
+      </FormControl>
+    </Grid>
+  );
+};
 
 const SiteBlockOptions: React.FC<{}> = ({}) => {
   return <Box></Box>;
---
+++ src/utils/blockIcons.ts
@@ -0,0 +1,32 @@
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaTiktok,
+  FaWhatsapp,
+  FaTelegram,
+  FaYoutube,
+  FaDiscord,
+  FaTwitch,
+  FaAmazon,
+  FaReddit,
+  FaFacebookMessenger,
+} from "react-icons/fa";
+import { TbBrandNetflix, TbBrandDisney } from "react-icons/tb";
+
+export const blockIcons = {
+  facebook: FaFacebook,
+  twitter: FaTwitter,
+  instagram: FaInstagram,
+  tiktok: FaTiktok,
+  whatsapp: FaWhatsapp,
+  telegram: FaTelegram,
+  youtube: FaYoutube,
+  discord: FaDiscord,
+  twitch: FaTwitch,
+  amazon: FaAmazon,
+  netflix: TbBrandNetflix,
+  disneyplus: TbBrandDisney,
+  reddit: FaReddit,
+  messenger: FaFacebookMessenger,
+};
